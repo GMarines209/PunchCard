61859df17b62a40efa0f8345b833bfb88d8b11f8
GMarines209 <gabrielmarines209@gmail.com>
2026-04-27 15:52:36 -0400

    inital database creation schema

---
+++ database.py
@@ -0,0 +1,49 @@
+import sqlite3 
+
+DB_FILENAME = 'fighters.db'
+
+def get_connection():
+    conn = sqlite3.connect(DB_FILENAME)
+    return conn
+
+
+def init_db():
+
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute("""CREATE TABLE IF NOT EXISTS fighters (
+            
+            fighterid INTEGER PRIMARY KEY,
+            name TEXT,
+            profileurl TEXT,
+            nickname TEXT,
+            height REAL,
+            weightclass TEXT,
+            reach REAL,
+            stance TEXT
+            )""")
+
+    conn.commit()
+
+    c.execute("""CREATE TABLE IF NOT EXISTS fighter_stats (
+            
+            statsid INTEGER PRIMARY KEY,
+            wins INTEGER,
+            losses INTEGER,
+            draws INTEGER,
+            nocontest INTEGER,
+            lastchecked TEXT,
+            SLpM REAL,
+            StrAcc INTEGER,
+            SApM REAL,
+            StrDef INTEGER,
+            TdAvg REAL,
+            TdAcc INTEGER,
+            TdDef INTEGER,
+            SubAvg REAL,
+            trackfighter INTEGER,
+            FOREIGN KEY(trackfighter) REFERENCES fighters(fighterid)
+            )""")
+
+    conn.commit()
